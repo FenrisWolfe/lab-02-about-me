799f7836ceab16728d95ba64672bab64344a05eb
Fredric T. Rapp Jr <fred.rapp@gmail.com>
2017-04-05 16:11:49 -0700

    Inititial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+//variable declaration and assignment;
+var cat = "Oscar";
+var age = 15;
+var userCat;
+
+userCat = prompt("What is the name of your cat?");
+userCatAge = prompt("how old is your cat?");
+var userCatjedi = confirm("is your cat a jedi?");
+var userCatForce;
+
+if (userCat === "Obi") {
+  userCatForce = true;
+
+} else {
+  userCatForce = false;
+}
+
+//strings, string methods (length and substr)
+
+
+//numbers and operands
+
+
+//confirm, prompt, console
+console.log("My cat's name is: " + cat);
+console.log("you're cat's name is: ");
+console.log("he is this many years old:");
+console.log(userCat + "is " + userCatAge + " years old.");
+console.log(userCat + " can use the force: " + userCatForce);
+console.log(age);
+console.log(turtle);
+
+//booleans, if/else, comparison operators
